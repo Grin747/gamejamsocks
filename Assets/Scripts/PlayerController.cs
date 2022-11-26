02277ad2f84658e9b0efd51787ffe06030c3f9f4
ReedKat <goshaidze03@gmail.com>
2022-11-26 21:50:11 +0500

    Feature realized in PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 
 public abstract class PlayerController : MonoBehaviour
 {
+    public enum MovementType
+    {
+        keyboard,
+        controller
+    }
+
+    public MovementType movementType;
+    public CharacterController characterController;
+
     private Rigidbody2D _rb;
     [SerializeField] private float speed = 15f;
     [SerializeField] private float jumpForce = 10f;
     private bool _isJump;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
+    private bool isOnGround;
+    private float x;
+    private Vector2 velocity;
 
     void Start()
     {
@@ -19,16 +31,46 @@ public abstract class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && IsDownGround())
+        if (movementType == MovementType.keyboard)
         {
-            _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            if (Input.GetButtonDown("Jump") && IsDownGround())
+            {
+                _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+
+            //keyboardmovement
+            x = Input.GetAxis("Horizontal");
+
+            Vector2 move = transform.right * x;
+
+            // characterController.Move(move * speed * Time.deltaTime);
+
+            // characterController.Move(velocity * Time.deltaTime);
+            float horizontal = x * speed;
+            _rb.AddForce(new Vector2(horizontal, 0));
+        }
+        else
+        {
+            if(movementType == MovementType.controller)
+            {
+                //keyboardmovement
+                x = Input.GetAxis("HorizontalGamepad");
+
+                if (Input.GetButtonDown("JumpGamepad") && IsDownGround())
+                {
+                    _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                }
+                
+                float horizontal = x * speed;
+                _rb.AddForce(new Vector2(horizontal, 0));
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        var horizontal = Input.GetAxis("Horizontal") * speed;
-        _rb.AddForce(new Vector2(horizontal, 0));
+        //var horizontal = Input.GetAxis("Horizontal") * speed;
+        //_rb.AddForce(new Vector2(horizontal, 0));
     }
 
     private bool IsDownGround() => Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
