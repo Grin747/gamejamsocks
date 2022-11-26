7711706b4c6438c4d68ec0453bd55dd699fd2197
Arseny <20mineslend14@gmail.com>
2022-11-27 01:35:32 +0500

    player action

--- Assets/Scripts/CollectingPlayer.cs
+++ Assets/Scripts/CollectingPlayer.cs
@@ -1,3 +1,12 @@
+using UnityEngine;
+
 public class CollectingPlayer : PlayerController
 {
+    protected override void PlayerAction()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("collect");
+        }
+    }
 }
