e9dac7f6e499280f9ecce50a405d72b163227d9a
ReedKat <goshaidze03@gmail.com>
2022-11-27 15:59:10 +0500

    Dialogues Well Done!

--- Assets/Scripts/DialogueWindow.cs
+++ Assets/Scripts/DialogueWindow.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DialogueWindow : MonoBehaviour
 {
-    [SerializeField]public TextMeshProUGUI textComponent;
+    [SerializeField] public TextMeshProUGUI textComponent;
+    [SerializeField] public Image imageComponent;
     public string[] lines;
+    public int[] indexesToSwapPicture;
+    public Sprite[] sprites;
     public float textSpeed;
 
     private int index;
+    private int spriteIndex;
     
     // Start is called before the first frame update
     void Start()
@@ -55,12 +62,14 @@ public class DialogueWindow : MonoBehaviour
         if (index < lines.Length - 1)
         {
             index++;
+            if(indexesToSwapPicture.Contains(index)) imageComponent.sprite = sprites[spriteIndex++];
             textComponent.text = string.Empty;
             StartCoroutine(TypeLine());
         }
         else
         {
             gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
